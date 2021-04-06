c97f78582825cd01ea5b99665f455b514271b3ca
Karmar <chuongchung.it@gmail.com>
2021-04-06 16:15:24 +0700

    02 - My First React JS App

--- myfirstreactapp/src/App.js
+++ myfirstreactapp/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Hello World !
+          Learn React - Hello World and Hello Karmar
         </a>
       </header>
     </div>
