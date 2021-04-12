472758a0cadf73cf1e55e9b6a0b0b7aeacc1a0e4
Karmar <chuongchung.it@gmail.com>
2021-04-12 18:09:18 +0700

    07 - Demo Form

---
+++ myfirstreactapp/src/MyForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "" };
+  }
+  myChangeHandler = (event) => {
+    console.log("myChangeHandler", event);
+    this.setState({ username: event.target.value });
+  };
+  render() {
+    let header = "";
+    if (this.state.username) {
+      header = <h1>Hello {this.state.username}</h1>;
+    } else {
+      header = "";
+    }
+
+    return (
+      <form>
+        {header}
+        {/* <h1>Hello {this.state.username}</h1> */}
+        <p>Enter your name:</p>
+        <input type="text" onChange={this.myChangeHandler} />
+      </form>
+    );
+  }
+}
+
+export default MyForm;
